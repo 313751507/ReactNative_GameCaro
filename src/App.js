dac3fea14fcea0c6cc1c050b3c6e3ce4b91e16f2
batru96 <gietbarca@gmail.com>
2017-10-07 09:53:08 +0700

    Add socket.io

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import SocketIOClient from 'socket.io-client';
 import MainStack from './Router';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.socket = SocketIOClient('http://192.168.0.106:3000');
+  }
+  componentDidMount() {
+    console.log('MEO');
+  }
   render() {
     return <MainStack />;
   }
